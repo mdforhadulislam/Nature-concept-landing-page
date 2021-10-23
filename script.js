c1e883d3451785e17a67cbe058283eae83277e2e
MD Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2021-10-23 18:41:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load',function(){
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+  });
+
+  $('.amount input').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500,
+      'linear'
+    );
+
+  });
+
+});
